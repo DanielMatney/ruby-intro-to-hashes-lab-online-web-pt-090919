893462be557edad3801652ff9964110a72fd5901
Daniel Matney <danieljmatney@gmail.com>
2019-09-22 04:09:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,6 +26,12 @@ monopoly = {
 	      :two_pieces_owned => 50,
 	      :three_pieces_owned => 75,
 	      :four_pieces_owned => 100
+	    },
+	    :names => {
+	      :reading_railroad => {"mortgage_value" => "$100"},
+	      :pennsylvania_railroad => {"mortgage_value" => "$200"},
+	      :b_and_o_railroad => {"mortgage_value" => "$400"},
+	      :shortline => {"mortgage_value" => "$800"}
 	    }
 	  }
 	}
